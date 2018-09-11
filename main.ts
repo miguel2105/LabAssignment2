3375a87453783b3c77486fa939193c8baa13b044
Lucas Phan <devmachine2@DevMachine2s-MacBook-Pro.local>
2018-09-11 16:36:15 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,19 @@
+
+
+class MyFirstProgram {
+  constructor(...argArray: Array<any>) {
+    this.printArgs(argArray);
+  }
+  printArgs(argArray) {
+    console.log(argArray);
+  }
+  runFirst(callback: Function) {
+    console.log('this method runs first...');
+  }
+  rundLast() {
+    console.log('this method runs last');
+  }
+}
+
+const newInstance = new MyFirstProgram("a", "b", "c");
+
